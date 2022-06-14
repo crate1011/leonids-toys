a53a9eb8cba8a76b9517ec6b791d563394f8aa4a
Christian Suriano <cratea113@gmail.com>
2022-06-13 22:37:42 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let leonToys = ["balls", "puppets", "rocks"]
+
+let name = "leonid's"
+
+let balls = 10;
+let puppets = null;
+let rocks = 10;
+
+if (balls <= 10) {
+    console.log(`${name} Balls are in stock`)
+} else {
+    console.log("sorry, no balls")
+}
+
+if (puppets >= 10) {
+    console.log(`${name} puppets are in stock`)
+} else {
+    console.log("sorry, no puppets")
+};
+
+if (rocks <= 10) {
+    console.log(`${name} Rockss are in stock`)
+} else {
+    console.log("sorry, no rocks")
+}
+
+const allToys = 10.99;
+let leonid = true;
+
+if (leonid === true) {
+    console.log("dont buy from the butthead")
+}
